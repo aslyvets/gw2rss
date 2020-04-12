6d2af3754515b43a8e0887fd1faa2c95eb6a979c
aslyvets <slivets.aleksay@gmail.com>
2020-04-12 12:01:05 +0200

    add power to decide

--- src/main/kotlin/com/jedicoder/StaticController.kt
+++ src/main/kotlin/com/jedicoder/StaticController.kt
@@ -10,6 +10,7 @@ import com.squareup.okhttp.Request
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.ModelAndView
+import kotlin.random.Random
 
 
 @RestController
@@ -34,17 +35,22 @@ class StaticController {
         }
     }
 
-    @RequestMapping("/playGw2", "/")
+    @RequestMapping("/playGw2")
     fun playGw2(): ModelAndView {
         val embedUrl = getGif("not today")
         val answer = "Not today!"
         return ModelAndView(INDEX, mapOf(GIF to embedUrl, ANSWER to answer))
     }
 
+    @RequestMapping("/")
+    fun random(): ModelAndView {
+        return if (Random.nextBoolean()) playGw2() else toDrinkBeer()
+    }
+
     @RequestMapping("/toDrinkBeer")
     fun toDrinkBeer(): ModelAndView {
         val embedUrl = getGif("beer")
-        val answer = "Yes!"
+        val answer = "Drink beer!"
         return ModelAndView(INDEX, mapOf(GIF to embedUrl, ANSWER to answer))
     }
 }
